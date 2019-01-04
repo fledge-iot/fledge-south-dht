8f0d9b90904a4908d59a6cb831eec3f1938c8973
Amandeep Singh Arora <aman@dianomic.com>
2019-01-04 16:01:24 +0530

    FOGL-2213: plugin_reconfigure fix

--- plugin.cpp
+++ plugin.cpp
@@ -98,7 +98,7 @@ Reading plugin_poll(PLUGIN_HANDLE *handle)
 void plugin_reconfigure(PLUGIN_HANDLE *handle, string& newConfig)
 {
 ConfigCategory	conf("dht", newConfig);
-DHT11 		*dht11 = (DHT11*)handle;
+DHT11 		*dht11 = (DHT11*)*handle;
 
 	if (conf.itemExists("asset"))
                 dht11->setAssetName(conf.getValue("asset"));

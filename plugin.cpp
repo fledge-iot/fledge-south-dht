30a88e8b13fd06df8b3e67738708c478f0978ba1
ashish-jabble <ashish@dianomic.com>
2019-02-25 16:17:58 +0530

    project name changed to dht11_V2

--- plugin.cpp
+++ plugin.cpp
@@ -20,12 +20,13 @@
 #include <version.h>
 
 using namespace std;
+#define PLUGIN_NAME "dht11_V2"
 
 /**
  * Default configuration
  */
 #define CONFIG  "{\"plugin\" : { \"description\" : \"DHT11 C south plugin\", " \
-                        "\"type\" : \"string\", \"default\" : \"dht11\", \"readonly\": \"true\" }, " \
+                        "\"type\" : \"string\", \"default\" : \"" PLUGIN_NAME "\", \"readonly\": \"true\" }, " \
                 "\"asset\" : { \"description\" : \"Asset name\", "\
                         "\"type\" : \"string\", \"default\" : \"dht11\", \"order\": \"1\", " \
                         "\"displayName\": \"Asset Name\" }, " \
@@ -44,9 +45,9 @@ extern "C" {
  * The plugin information structure
  */
 static PLUGIN_INFORMATION info = {
-	"DHT11",                  // Name
+	PLUGIN_NAME,              // Name
 	VERSION,                  // Version
-	0,    			  // Flags
+	0,    			          // Flags
 	PLUGIN_TYPE_SOUTH,        // Type
 	"1.0.0",                  // Interface version
 	CONFIG                    // Default configuration
@@ -99,7 +100,7 @@ Reading plugin_poll(PLUGIN_HANDLE *handle)
 void plugin_reconfigure(PLUGIN_HANDLE *handle, string& newConfig)
 {
 ConfigCategory	conf("dht", newConfig);
-DHT11 		*dht11 = (DHT11*)*handle;
+DHT11 *dht11 = (DHT11*)*handle;
 
 	if (conf.itemExists("asset"))
                 dht11->setAssetName(conf.getValue("asset"));
